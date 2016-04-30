dd8d9d2a07756c44c674177c0bbb86f342a25dfe
petraohlin8 <petraohlin8@gmail.com>
2016-04-30 18:29:31 +0200

    searching for author_name working

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ var express = require('express');
 var pg = require('pg');
 var app = express();
 var db = require("./scripts/db.js");
+var bodyParser = require('body-parser');
 
-app.set('port', (process.env.PORT || 5000));
+
+var db = require("./scripts/db.js");app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.get('/', function(request, response) {
   response.render('pages/index');
@@ -18,6 +23,12 @@ app.get('/', function(request, response) {
 app.get('/getTest', function(req, res) {
 	db.getTags(req, res);	
 });
+
+app.post('/getSearch', function(req, res) {
+	db.getSearch(req, res);	
+});
+
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
--- scripts/db.js
+++ scripts/db.js
@@ -2,8 +2,9 @@ module.exports = {
   getTags: function(req, res) {    
         var pg = require('pg');  
       
-        var conString = "postgres://mattiaspalmgren:@localhost/mattiaspalmgren";
-
+        //var conString = "postgres://mattiaspalmgren:@localhost/mattiaspalmgren";
+        var conString = "postgres://petraohlin8:@localhost/tidbit";
+        
         var client = new pg.Client(conString);
         client.connect(function(err) {
           if(err) {
@@ -17,5 +18,34 @@ module.exports = {
             client.end();
           });
         });
-  }  
+  },
+
+  getSearch: function(req, res) {    
+      var pg = require('pg');  
+    
+      //var conString = "postgres://mattiaspalmgren:@localhost/mattiaspalmgren";
+      var conString = "postgres://petraohlin8:@localhost/tidbit";
+      var queryString = "SELECT * FROM authors WHERE author_name = '" + req.body.search + "'"
+
+      console.log(req.body.search);
+      console.log(queryString);
+
+      
+      var client = new pg.Client(conString);
+      client.connect(function(err) {
+        if(err) {
+          return console.error('could not connect to postgres', err);
+        }
+        client.query(queryString, function(err, result) {
+          if(err) {
+            return console.error('error running query', err);
+          }
+
+          res.send(result);
+          client.end();
+        });
+      }); 
+  }
+
+
 };
